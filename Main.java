d9994f9b23c2a5f69ad6fba6f37114724a8373f8
Arpan <arpanpramanik219@gmail.com>
2024-08-13 21:10:12 +0530

    add main

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
-//import tasks.*;
+
+import java.time.LocalDateTime;
+
+import tasks.Task;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Task task1 = new Task(1, "Task 1", "Description 1", "Category 1", "Pending", 1, LocalDateTime.parse("2021-12-31T23:59:59"));
+        Task task2 = new Task(1, "Task 2", "Description 2", "Category 2", "Pending", 1, LocalDateTime.parse("2021-12-30T23:59:59"));
+        task1.displayTask();
+        task2.displayTask();
+        task1.editTask("Task 1", "Description 1", "Category 1", "Done", 1, LocalDateTime.parse("2021-12-31T23:59:59"));
         
+        task1.displayTask();
     }
 }
--- tasks/Task.java
+++ tasks/Task.java
@@ -1,4 +1,46 @@
 package tasks;
 
+import java.time.LocalDateTime;
+
 public class Task{
+    int id;
+    String title;
+    String description;
+    String category;
+    String status;
+    int priority;
+    LocalDateTime deadline;
+    public Task(int id, String title, String description, String category, String status, int priority, LocalDateTime deadline){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.status = status;
+        this.priority = priority;
+        this.deadline = deadline;
+    }
+    public void displayTask(){
+        System.out.println("Task ID: " + id);
+        System.out.println("Title: " + title);
+        System.out.println("Description: " + description);
+        System.out.println("Category: " + category);
+        System.out.println("Status: " + status);
+        System.out.println("Priority: " + priority);
+        System.out.println("Deadline: " + deadline);
+    }
+    public void editTask(String title, String description, String category, String status, int priority, LocalDateTime deadline){
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.status = status;
+        this.priority = priority;
+        this.deadline = deadline;
+    }
+    public void markAsDone(){
+        this.status = "Done";
+    }
+    public void removeTask(int id){
+        // remove task from the list
+        this.id = 0;
+    }
 }
